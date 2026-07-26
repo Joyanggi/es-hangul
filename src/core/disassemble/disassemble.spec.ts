1a3c85c48b4a4f8d43d8c53a90dbefc78cc2a85b
Joyanggi <jyg8033@gmail.com>
2026-07-26 16:53:32 +0900

    test: getChoseong, disassemble에 비한글 문자 경계 케이스 테스트 추가

--- src/core/disassemble/disassemble.spec.ts
+++ src/core/disassemble/disassemble.spec.ts
@@ -20,4 +20,28 @@ describe('disassemble', () => {
   it('ㅘ', () => {
     expect(disassemble('ㅘ')).toEqual('ㅗㅏ');
   });
+
+  describe('한글이 아닌 문자 처리', () => {
+    it('영어는 분해하지 않고 그대로 유지한다.', () => {
+      expect(disassemble('abc')).toEqual('abc');
+      expect(disassemble('값abc')).toEqual('ㄱㅏㅂㅅabc');
+    });
+
+    it('특수문자는 그대로 유지한다.', () => {
+      expect(disassemble('값!@#')).toEqual('ㄱㅏㅂㅅ!@#');
+    });
+
+    it('한글이 아닌 다른 언어는 그대로 유지한다.', () => {
+      expect(disassemble('りんご')).toEqual('りんご');
+      expect(disassemble('中國')).toEqual('中國');
+    });
+
+    it('숫자는 그대로 유지한다.', () => {
+      expect(disassemble('값123')).toEqual('ㄱㅏㅂㅅ123');
+    });
+
+    it('빈 문자열은 빈 문자열을 반환한다.', () => {
+      expect(disassemble('')).toEqual('');
+    });
+  });
 });
--- src/core/getChoseong/getChoseong.spec.ts
+++ src/core/getChoseong/getChoseong.spec.ts
@@ -25,4 +25,29 @@ describe('getChoseong', () => {
   it('keepNonHangul이 true이면 숫자 등 비한글을 유지한다.', () => {
     expect(getChoseong('네이버123', { keepNonHangul: true })).toBe('ㄴㅇㅂ123');
   });
+
+  describe('한글이 아닌 문자 처리', () => {
+    it('영어는 제거한다.', () => {
+      expect(getChoseong('apple')).toBe('');
+      expect(getChoseong('사과apple')).toBe('ㅅㄱ');
+    });
+
+    it('특수문자는 제거한다.', () => {
+      expect(getChoseong('사과!@#')).toBe('ㅅㄱ');
+    });
+
+    it('한글이 아닌 다른 언어는 제거한다.', () => {
+      expect(getChoseong('りんご')).toBe('');
+      expect(getChoseong('中國')).toBe('');
+    });
+
+    it('빈 문자열은 빈 문자열을 반환한다.', () => {
+      expect(getChoseong('')).toBe('');
+    });
+
+    it('keepNonHangul이 true이면 영어와 특수문자를 유지한다.', () => {
+      expect(getChoseong('사과apple', { keepNonHangul: true })).toBe('ㅅㄱapple');
+      expect(getChoseong('사과!@#', { keepNonHangul: true })).toBe('ㅅㄱ!@#');
+    });
+  });
 });
